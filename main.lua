8a9ad000c89ce8aca460ad555a7e89ded00ddd01
Dave Nicholson <fatnic388@gmail.com>
2016-11-18 18:57:31 +0000

    some nifty nighting and swapped back to objectlayer collison rects

--- main.lua
+++ main.lua
@@ -5,6 +5,8 @@ sti = require 'ext.sti'
 bump = require 'ext.bump'
 vec = require 'ext.hump.vector'
 
+LightWorld = require 'ext.lightworld'
+
 -- compontents
 require 'components.sprite'
 require 'components.position'
@@ -42,11 +44,23 @@ function love.load()
     engine = Engine()
     world = bump.newWorld(16)
 
+    light = LightWorld({
+        ambient = {20, 20, 20},
+        glowBlur = 0.0,
+    })
+    light:setShadowBlur(10)
+
     engine:addSystem(SpriteDrawingSystem())
     engine:addSystem(PlatformerSystem(world))
 
-    map = sti('assets/maps/grid.lua', {'bump'})
-    map:bump_init(world)
+    map = sti('assets/maps/grid.lua')
+
+    local collisions = map.layers['collision'].objects
+
+    for _, c in pairs(collisions) do
+        world:add({properties=c.properties}, c.x, c.y, c.width, c.height)
+        light:newRectangle(c.x + c.width/2 + 1, c.y + c.height/2 + 1, c.width - 2, c.height - 2)
+    end
     
     -- player entity
     player = Entity()
@@ -58,8 +72,8 @@ function love.load()
     world:add({}, -5, 0, 5, love.graphics.getHeight())
     world:add({}, love.graphics.getWidth(), 0, 5, love.graphics.getHeight())
 
-    love.graphics.setBackgroundColor(255, 255, 255)
-
+    plight = light:newLight(0, 0, 255, 127, 63, 300)
+    
 end
 
 function love.update(dt)
@@ -69,10 +83,16 @@ function love.update(dt)
     if Input:down    'left'  then player:get("Platformer"):moveLeft(dt) end
     if Input:pressed 'jump'  then player:get("Platformer"):jump(dt) end
 
+    plight:setPosition(player:get("Position").x + math.random(6,8), player:get("Position").y + 25)
+    plight:setRange(math.random(300, 400))
+    
+    light:update(dt)
     engine:update(dt)
 end
 
 function love.draw()
-    map:draw()
-    engine:draw()
+    light:draw(function()
+        map:draw()
+        engine:draw()
+    end)
 end
--- systems/platformer.lua
+++ systems/platformer.lua
@@ -17,6 +17,10 @@ function PlatformerSystem:update(dt)
         local sprite = e:get("Sprite")
         local platformer = e:get("Platformer")
         local position = e:get("Position")
+
+        local filter = function(item, other)
+            return 'slide'
+        end
         
         goal = {x=0, y=0}
         actual = {x=0, y=0}
@@ -34,10 +38,13 @@ function PlatformerSystem:update(dt)
 
         if goal.x ~= position.x or goal.y ~= position.y then 
 
-            actual.x, actual.y, cols, len = self.world:check(e, goal.x, goal.y)
+            actual.x, actual.y, cols, len = self.world:check(e, goal.x, goal.y, filter)
+            position.x, position.y = actual.x, actual.y
+            self.world:update(e, position.x, position.y)
 
             for _, c in pairs(cols) do
 
+                -- normal blocks
                 if c.normal.y == -1 then -- top of platform 
                     platformer.jumping = false 
                     platformer.grounded = true
@@ -46,11 +53,14 @@ function PlatformerSystem:update(dt)
                     platformer.velocity.y = 0 
                 end
 
+                if c.other.properties and c.other.properties.jumpboost then
+                    platformer.velocity.y = -platformer.jumpheight * 1.6
+                    platformer.jumping = true
+                    platformer.grounded = false
+                end
+                
             end
 
-            position.x, position.y = actual.x, actual.y
-
-            self.world:update(e, position.x, position.y)
         end
 
     end
